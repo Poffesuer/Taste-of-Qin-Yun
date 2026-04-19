5930c4f74e0b0fc70a8d496b17844e34b7ce63ad
HetarthP <hetarthparikh@gmail.com>
2026-04-19 18:02:31 -0400

    Add active cart indicator badge to navigation

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,15 @@ $base_url = '';
 if (strpos($_SERVER['REQUEST_URI'], '/taste-of-qin-yun') === 0) {
     $base_url = '/taste-of-qin-yun';
 }
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+$cart_count = 0;
+if (!empty($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $item) {
+        $cart_count += $item['qty'] ?? 1;
+    }
+}
 ?>
 <nav>
     <img src="<?php echo $base_url; ?>/images/logo.png" class="logo">
@@ -14,7 +23,10 @@ if (strpos($_SERVER['REQUEST_URI'], '/taste-of-qin-yun') === 0) {
     <?php else: ?>
         <a href="<?php echo $base_url; ?>/login/login.php">Login</a>
     <?php endif; ?>
-    <a href="<?php echo $base_url; ?>/cart/cart.php">
+    <a href="<?php echo $base_url; ?>/cart/cart.php" class="cart-container">
         <img src="<?php echo $base_url; ?>/images/cart.png" class="cart">
+        <?php if ($cart_count > 0): ?>
+            <span class="cart-badge"><?php echo $cart_count; ?></span>
+        <?php endif; ?>
     </a>
 </nav>
